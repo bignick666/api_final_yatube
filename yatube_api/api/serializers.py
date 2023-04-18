4f77222914779b4f4938ca5265db41b2592c23fd
Nikita Berstnev <bignick666@yandex.ru>
2023-04-18 21:45:15 +0300

    asdasd

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -62,14 +62,8 @@ class FollowSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     posts = serializers.StringRelatedField(many=True, read_only=True)
+
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'posts')
-
-
-
-
-
-
-
-
+        
