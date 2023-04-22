f4bdfc2af72468fc52668af7d98b05ebcf6b378b
Nikita Berstnev <bignick666@yandex.ru>
2023-04-22 22:34:53 +0300

    asdasdasd

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -12,9 +12,7 @@ from api.permissions import AuthorOrReadOnly
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (AuthorOrReadOnly,
-                          permissions.IsAuthenticatedOrReadOnly,
-                          permissions.IsAuthenticated)
+    permission_classes = (AuthorOrReadOnly,)
     pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
@@ -29,9 +27,7 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (AuthorOrReadOnly,
-                          permissions.IsAuthenticatedOrReadOnly,
-                          permissions.IsAuthenticated)
+    permission_classes = (AuthorOrReadOnly,)
 
     def get_queryset(self):
         post_id = self.kwargs.get("post_id")
