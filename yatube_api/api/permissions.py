6a12223f131a63eee06019ff63b365799ce47c8e
Nikita Berstnev <bignick666@yandex.ru>
2023-04-22 22:39:16 +0300

    asdasdasd

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,13 +2,6 @@ from rest_framework import permissions
 
 
 class AuthorOrReadOnly(permissions.BasePermission):
-    # Не совсем понимаю как избавиться от
-    # request.user.is_authenticated
-    # Пытаюсь добавлять пермишн во вьюхах, но
-    # Вылезает куча ошибок
-    def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS
-
     def has_object_permission(self, request, view, obj):
         return (obj.author == request.user
                 or request.method in permissions.SAFE_METHODS)
