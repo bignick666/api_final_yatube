fd72fe85928561461e88e766e67e17cf7a3edcac
Nikita Berstnev <bignick666@yandex.ru>
2023-04-22 23:18:44 +0300

    asdasdasd

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -30,10 +30,14 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = (AuthorOrReadOnly,
                           permissions.IsAuthenticatedOrReadOnly)
 
+    def get_post(self):
+        return get_object_or_404(
+            Post,
+            pk=self.kwargs.get('post_id')
+        )
+
     def get_queryset(self):
-        post_id = self.kwargs.get("post_id")
-        post = get_object_or_404(Post, id=post_id)
-        return post.comments.all()
+        return self.get_post().comments
 
     def perform_create(self, serializer):
         post = get_object_or_404(Post, id=self.kwargs.get("post_id"))
